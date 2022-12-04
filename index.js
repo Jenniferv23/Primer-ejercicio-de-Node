3af9460d7a7d55151b6a7997907f628923f09741
Jenniferv23 <jvanegas369@misena.edu.co>
2022-12-03 19:23:17 -0500

    Se carga primer ejercicio de Node

---
+++ index.js
@@ -0,0 +1,9 @@
+// Este es el mensaje de bienvenida
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programaciòn del mundo");
+
+// Esta es la parte intermedia de nuestro proyecto
+// Esta es la parte de chequeos necesarios
+// Esta es la ùltima linea de saludo
+
+// Este es el mensaja de despedida
+console.log("Chao")
